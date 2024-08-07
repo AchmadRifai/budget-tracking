0088c0ab75323e5deddb8fdd7f6e7da032872d77
achmad.rifai <achmad.rifai@aryanoble.co.id>
2024-08-07 11:42:57 +0700

    db connect

--- be/db/db.go
+++ be/db/db.go
@@ -8,8 +8,10 @@ import (
 	"gorm.io/gorm"
 )
 
-func dbConnect() (*gorm.DB, error) {
+func DbConnect() (*gorm.DB, error) {
 	host, user, dbname, port, password := os.Getenv("PSQL_HOST"), os.Getenv("PSQL_USER"), os.Getenv("PSQL_DB"), os.Getenv("PSQL_PORT"), os.Getenv("PSQL_PASSWORD")
-	database, err := gorm.Open(postgres.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", host, user, password, dbname, port)), &gorm.Config{})
+	psqlOpen := postgres.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", host, user, password, dbname, port))
+
+	database, err := gorm.Open(psqlOpen, &gorm.Config{})
 	return database, err
 }
